185907467b0f768d237da081bb4a88efd9bc749b
Ramazan_Khasanov <xasanov_ramazan@bk.ru>
2022-09-30 11:46:02 +0300

    task 1

---
+++ Figure.cs
@@ -0,0 +1,34 @@
+﻿namespace Square
+{
+    abstract public class Figure
+    {
+        abstract public double Square();
+    }
+
+    public class Circle: Figure
+    {
+
+        private double radius;
+        public Circle(double radius) => this.radius = radius;
+        public override double Square() => Math.PI * Math.Pow(radius, 2);
+
+    }
+
+    public class Triangle: Figure
+    {
+        private double a, b, c;
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double Square()
+        {
+            double p = (a + b + c) / 2.0;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+    }
+}
