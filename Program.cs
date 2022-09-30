adc1063ae34f5aeef103c42e5c502426414f5c1b
Ramazan_Khasanov <xasanov_ramazan@bk.ru>
2022-09-30 11:41:56 +0300

    task 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Square;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Площадь круга равна {0: .###}", new Circle(25).Square());
+            Console.WriteLine("Площадь треугольника равна {0: .###}", new Triangle(14, 13, 18).Square());
+        }
+    }
+}
